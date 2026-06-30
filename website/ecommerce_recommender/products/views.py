5591033e9a125eeebbfcb04056ef4119c0665949
codexMonitor <tranbduy189@gmail.com>
2026-06-30 12:19:11 +0700

    Added backup products.csv

--- website/ecommerce_recommender/products/views.py
+++ website/ecommerce_recommender/products/views.py
@@ -4,8 +4,8 @@ import pandas as pd
 
 def products(request):
 
-    # df = pd.read_csv("D:\GitHub\E-commerce-Product-Recommendation\data\processed\products.csv")
+    df = pd.read_csv("D:\GitHub\E-commerce-Product-Recommendation\data\processed\products.csv")
     
-    # return render(request, "products/products.html", {"products": df.to_dict("records")})
+    return render(request, "products/products.html", {"products": df.to_dict("records")})
 
-    return render(request, "products/products.html")
+    # return render(request, "products/products.html")
